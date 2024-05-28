aa6e166326d08571a32314b23898eb25b3dbe897
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-05-28 20:53:52 -0300

    refactor: improve base layouts

--- frontend/layouts/login.vue
+++ frontend/layouts/login.vue
@@ -1,18 +1,17 @@
 <template>
     <div>
-        <header
+        <nav
             class="fixed top-0 left-0 w-full flex items-center justify-between py-20 px-[10%]"
         >
             <NuxtLink to="/" class="text-24 font-600">
                 SiN.music
             </NuxtLink>
 
-            <nav class="navbar">
-                <NuxtLink to="/" class="text-18 font-500">
-                    Home
-                </NuxtLink>
-            </nav>
-        </header>
+            <Button to="/">
+                Home
+                <Icon icon-name="bx bx-home-alt-2" />
+            </Button>
+        </nav>
 
         <div class="flex flex-col justify-center items-center h-screen bg-login bg-cover bg-center">
             <div
