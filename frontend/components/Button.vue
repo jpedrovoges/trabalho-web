472dfdac3c0fa0ece7a22c0d896b6f2faf9b0c57
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-05-28 20:53:02 -0300

    refactor: improve button component

--- frontend/components/Button.vue
+++ frontend/components/Button.vue
@@ -1,9 +1,18 @@
 <template>
-    <button
+    <component
+        :is="to ? NuxtLink : 'button'"
+        :to="to"
         class="inline-flex items-center justify-center
-            h-48 bg-gray-50 rounded-32 font-600 text-black
+            max-h-48 px-24 py-12 gap-12 bg-gray-50 rounded-32 font-600 text-black
             hover:bg-gray-300 transition-colors shadow-[0_0_10px_rgba(0,0,0,0.2)]"
     >
         <slot />
-    </button>
+    </component>
 </template>
+
+<script setup lang="ts">
+    import type { RouteLocationRaw } from 'vue-router'
+    import { NuxtLink } from '#components'
+
+    defineProps<{ to?: RouteLocationRaw }>()
+</script>
