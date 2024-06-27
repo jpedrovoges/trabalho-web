6bd8d325fd82e31e62cf542fab4757611ef5e277
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-06-26 23:57:06 -0300

    refactor: hash user password

--- backend/app/Models/User.ts
+++ backend/app/Models/User.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, beforeSave } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class User extends BaseModel {
     @column({ isPrimary: true })
@@ -13,6 +14,12 @@ export default class User extends BaseModel {
 
     @column({ serializeAs: null })
     public password: string
+
+    @beforeSave()
+    public static async hashPassword(user: User) {
+        if (user.$dirty.password)
+            user.password = await Hash.make(user.password)
+    }
 
     @column.dateTime({ autoCreate: true })
     public createdAt: DateTime
