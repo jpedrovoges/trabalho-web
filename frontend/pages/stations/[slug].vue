c3e1494d574065756662fac09b9a525e1871ad1c
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-07-01 19:35:13 -0300

    chore: remove console.log

--- frontend/pages/stations/[slug].vue
+++ frontend/pages/stations/[slug].vue
@@ -10,7 +10,5 @@
     const stations = inject<Ref<Station[]>>('stations')
     const { slug } = useRoute().params as unknown as { slug: string }
 
-    console.log(stations?.value)
-
     const station = stations?.value?.find(station => station.slug === slug)
 </script>
